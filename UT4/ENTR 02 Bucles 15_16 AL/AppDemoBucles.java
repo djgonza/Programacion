1e76670168fc9ab41e47666f62978a6d028d8776
David Jimenez Gonzalez <davidjimenezgonza@gmail.com>
2015-11-13 14:23:33 +0100

    Actualizacion

---
+++ UT4/ENTR 02 Bucles 15_16 AL/AppDemoBucles.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+/**
+ * Incluye todo el código dentro del main()
+ * No declares ningún nuevo método en esta clase ni atributos
+ *  
+ */
+public class AppDemoBucles
+{
+
+    private static Scanner teclado = new Scanner(System.in);
+
+
+    private static int validarNumero (int numero, int grosor, String mensaje) {
+
+        //validamos el numero
+        while(numero < grosor){
+            System.out.println(mensaje);
+            numero = teclado.nextInt(); 
+        }
+
+        
+
+        return numero;
+    } 
+
+    /**
+     *  Punto de entrada a la aplicación
+     *  define y crea el teclado, 
+     *  crea un objeto DemoBucles y:
+     *  - ejecuta el método numerosComoSumaPotenciasDe2()
+     *  - haz una pausa y borra la pantalla
+     *  - pide al usuario un valor para el grosor de la H - Valida con un bucle while  que este
+     *      valor es positivo
+     *  - pide al usuario un valor para la anchura de la H. Valida con un bucle while 
+     *      que este valor es positivo y superior al grosor
+     *   - pide al usuario un valor para la altura de la H. Valida con un bucle while 
+     *      que este valor es positivo y superior al grosor
+     *   - muestra la letra H
+     *  
+     */
+    public static void main(String[] args)
+    {
+        
+        Utilidades.borrarPantalla();
+
+        //creamos el objeto
+        DemoBucles demo = new DemoBucles();
+
+        //llamaos al primer metodo
+        demo.numerosComoSumaPotenciasDe2();
+        
+        //limpiamos pantalla
+        Utilidades.hacerPausa();
+        Utilidades.borrarPantalla();
+
+        //pedimos los numeros
+        System.out.println("Dame un nº positivo para el grosor");
+        int grosor = validarNumero (teclado.nextInt(), 1, "Grosor Incorrecto!!");
+
+        System.out.println("Dame un nº positivo y mayor que el grosor para la anchura");
+        int anchura = validarNumero (teclado.nextInt(), grosor, "Anchura Incorrecta!!");
+
+        System.out.println("Dame un nº positivo y mayor que el grosor para la altura");
+        int altura = validarNumero (teclado.nextInt(), grosor, "Altura Incorrecta");
+
+        //mostramos la L
+        demo.escribirLetraL(altura, anchura, grosor);
+        
+    }
+}
+
---
+++ UT4/ENTR 02 Bucles 15_16 AL/DemoBucles.java
@@ -0,0 +1,167 @@
+import java.util.Random;
+/**
+ *    
+ */
+public class DemoBucles
+{
+    private static final char SUMATORIO = '\u2211';
+    private static final char ASTERISCO = '*';
+    private Random generador;
+
+    /**
+     * Constructor  
+     */
+    public DemoBucles()
+    {
+        generador = new Random();
+    }
+
+    /*
+    *   Descomponer numeros  
+    */
+    public String descomponerNumero(int numero){
+
+        int i = 7;
+        String cadena = String.format("%10d = %3c", numero, SUMATORIO);
+        
+        while (numero > 0){
+            int potencia = (int) Math.pow(2,i);
+            if(potencia <= numero){
+                cadena += String.format("%4d", potencia);
+                numero -= potencia;
+            }
+            i--;
+        }
+
+        return cadena;
+    }
+
+    /**
+     *  Genera aleatorios (con la clase Random) ente 0 y 255 (inclusive)
+     *  y escribe cada aleatorio como suma de potencias de 2.
+     *  OJO! No hay que convertir previamente el nº a binario.
+     *  
+     *  Hay que parar el proceso al salir un 0 o después de haber generado 25 aleatorios
+     *  Los resultados se muestran formateados como indica la figura (ver enunciado)
+     *  
+     *  Cada 4 números mostrados se hace una pausa y luego se borra la pantalla
+     *  
+     *  255 = 128 + 64 + 32 + 16 + 8 + 4 + 2 + 1
+     *  34 = 32 + 2
+     *  129 = 128 + 1
+     *  
+     *  Usa solo bucles while
+     */
+    public void numerosComoSumaPotenciasDe2()
+    {
+        Utilidades.borrarPantalla();
+
+        int i = 1;
+        int aleatorio = 1;
+
+        while (i <= 25 && aleatorio != 0){
+            
+            //generamos el numero aleatorio y lo mostramos
+            System.out.println(
+                descomponerNumero(
+                    generador.nextInt(256)
+                )
+            );
+
+            /*//generamos el aleatorio
+            aleatorio = generador.nextInt(256);
+
+            //descomponemos el numero
+            int j = 7;
+            int numero = aleatorio;
+            String cadena = String.format("%10d = %3c", numero, SUMATORIO);
+            while (numero > 0){
+                int potencia = (int) Math.pow(2,j);
+                if(potencia <= numero){
+                    cadena += String.format("%4d", potencia);
+                    numero -= potencia;
+                }
+                j--;
+            }
+            
+            //mostrar por pantalla
+            System.out.println(cadena);*/
+            
+            //ciclos de 4
+            if(i%4==0){
+                Utilidades.hacerPausa();
+                Utilidades.borrarPantalla();
+            }
+            
+            //contador
+            i++;
+
+        }
+        
+        
+    }
+
+    /**
+     *  Con bucles for
+     *  
+     *  alto, ancho, grosor asumimos positivos y correctos
+     *  
+     *      Altura de la letra L: 9
+            *Anchura de la letra L: 5
+            *Grosor de la letra L: 3
+             * * *
+             * * *
+             * * *
+             * * *
+             * * *
+             * * *
+             * * * * *
+             * * * * *
+             * * * * *      
+            *Altura de la letra L: 5
+            *Anchura de la letra L: 3
+            *Grosor de la letra L: 1
+             * 
+             * 
+             * 
+             * 
+             * * * 
+     *
+     */
+    public void escribirLetraL(int altura, int anchura, int grosor)
+    {
+       
+        //System.out.println(altura);
+        //System.out.println(anchura);
+        //System.out.println(grosor);
+        
+        for(int i=1; i<=altura; i++){
+
+            if(i<= anchura/2)
+                escribirCaracter(ASTERISCO, grosor);
+            else
+                escribirCaracter(ASTERISCO, anchura);
+
+        }
+        
+    }
+
+    /**
+     *  escribe n veces el caracter  indicado (en 2 posiciones)
+     *  en la misma línea
+     *  
+     *  con bucles for 
+     */
+    private void escribirCaracter(char caracter, int n)
+    {
+        String cadena = "";
+        for(int i=1; i<=n; i++){
+            cadena += String.format("%2c", caracter);
+        }
+
+        System.out.println(cadena);
+        
+        
+    }
+
+}
