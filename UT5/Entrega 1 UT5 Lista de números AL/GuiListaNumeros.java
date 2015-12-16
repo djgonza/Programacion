1abce2e2ef87b1c9aed2354dfe9769524ffac86b
David Jimenez Gonzalez <davidjimenezgonza@gmail.com>
2015-12-16 19:42:24 +0100

    Entrega actualizada

---
+++ UT5/Entrega 1 UT5 Lista de números AL/GuiListaNumeros.java
@@ -0,0 +1,401 @@
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.List;
+import java.util.Arrays;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.KeyStroke;
+
+public class GuiListaNumeros extends JFrame  
+{
+    private static final int ANCHO = 600;
+    private static final int ALTO = 400;
+
+    private JButton btnAdd;
+
+    private JButton btnSegundoMinimo;
+    private JButton btnBorrarSegundosMinimos;
+    private JButton btnOrdenar;
+    private JButton btnDigitosMasGrandes;
+    private JButton btnMostrarLista;
+    private JButton btnVaciarLista;
+    private JButton btnClear;
+    private JTextField txtNumero;
+    private JTextArea areaTexto;
+
+    private ListaNumeros lista;
+
+    public GuiListaNumeros(String titulo, ListaNumeros lista)
+    {
+        super(titulo);
+        crearGui();
+        mostrarGui();
+        this.lista = lista;
+        mostrarLista();
+    }
+
+    /**
+     * crear los componenetes y anadirlos el contenedor del frame
+     */
+    private void crearGui()
+    {
+
+        JPanel pnlCentro = crearPanelCentral();
+        this.add(pnlCentro, BorderLayout.CENTER);
+
+        JPanel pnlEste = crearPanelDerecha();
+        this.add(pnlEste, BorderLayout.EAST);
+
+    }
+
+    /**
+     * crear el panel central que contiene dos paneles uno al norte (el que
+     * tenéis que crear) y otro al centro (el del área de texto)
+     */
+    private JPanel crearPanelCentral()
+    {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(crearPanelEntradaDatos(), BorderLayout.NORTH);
+        panel.add(crearPanelArea(), BorderLayout.CENTER);
+        return panel;
+    }
+
+    /**
+     * crear el panel de entrada de datos
+     */
+    private JPanel crearPanelEntradaDatos()
+    {
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("Tecle n", JLabel.RIGHT));
+        txtNumero = new JTextField(15);
+        txtNumero.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0)
+                {
+                    addNumero();
+
+                }
+            });
+        panel.add(txtNumero);
+
+        btnAdd = new JButton("Add numero");
+        btnAdd.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0)
+                {
+                    addNumero();
+
+                }
+            });
+        panel.add(btnAdd);
+
+        return panel;
+    }
+
+    /**
+     * panel que contiene el area de texto
+     *  
+     */
+    private JPanel crearPanelArea()
+    {
+        JPanel panel = new JPanel(new BorderLayout());
+        areaTexto = new JTextArea(30, 100);
+        areaTexto.setEditable(false);
+        JScrollPane scroll = new JScrollPane(areaTexto);
+        panel.add(scroll, BorderLayout.CENTER);
+        return panel;
+    }
+
+    /**
+     * crear y devolver panel de botones
+     */
+    private JPanel crearPanelDerecha()
+    {
+        JPanel panel = new JPanel(new GridLayout(0, 1, 3, 3));
+
+        btnSegundoMinimo = new JButton("Segundo minimo");
+        panel.add(btnSegundoMinimo);
+        btnSegundoMinimo.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0)
+                {
+                    segundoMinimo();
+
+                }
+
+            });
+
+        btnBorrarSegundosMinimos = new JButton("Borrar segundos minimos");
+        panel.add(btnBorrarSegundosMinimos);
+        btnBorrarSegundosMinimos.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0)
+                {
+                    borrarSegundosMinimos();
+
+                }
+
+            });
+
+        btnDigitosMasGrandes = new JButton("Digitos mas grandes");
+        panel.add(btnDigitosMasGrandes);
+        btnDigitosMasGrandes.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0)
+                {
+                    digitosMasGrandes();
+
+                }
+
+            });
+
+        btnOrdenar = new JButton("Ordenar lista");
+        panel.add(btnOrdenar);
+        btnOrdenar.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0)
+                {
+                    ordenar();
+
+                }
+
+            });
+
+        btnClear = new JButton("Limpiar area texto");
+        panel.add(btnClear);
+        btnClear.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0)
+                {
+                    clear();
+
+                }
+
+            });
+
+        btnMostrarLista = new JButton("Mostrar Lista");
+        panel.add(btnMostrarLista);
+        btnMostrarLista.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0)
+                {
+                    mostrarLista();
+
+                }
+
+            });
+
+        btnVaciarLista = new JButton("Vaciar lista");
+        panel.add(btnVaciarLista);
+        btnVaciarLista.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent arg0)
+                {
+                    vaciarLista();
+
+                }
+
+            });
+
+        return panel;
+    }
+
+    /**
+     * ubicar y mostrar frame
+     */
+    private void mostrarGui()
+    {
+        this.setSize(ANCHO, ALTO);
+        // this.pack();
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+    }
+
+    /**
+     *  anade un nuevo numero a la lista mostrando el estado actual de la lista
+     *  en el area de texto
+     *  
+     */
+    private void  addNumero()
+    {
+        clear();
+        String strNumero = txtNumero.getText();
+        if (strNumero.isEmpty())
+        {
+            areaTexto.append("Teclee n");
+
+        }
+        else
+        {
+            try
+            {
+                boolean exito = lista.addElemento(Integer.parseInt(strNumero));
+                if (!exito)
+                {
+                    areaTexto.append("\nLista completa, no se ha podido anadir");
+                }
+                mostrarLista();
+
+            }
+            catch (NumberFormatException e)
+            {
+                areaTexto.append("\nTeclee solo digitos numericos");
+            }
+
+        }
+        cogerFoco();
+    }
+
+    /**
+     *   Muestra en el area de texto el array con los digitos
+     *   mas grandes de los elementos de lista
+     *  
+     */
+    private void  digitosMasGrandes()
+    {
+        clear();  
+        mostrarLista();
+        areaTexto.append("\nEl nuevo objeto ListaNumeros contiene " +
+            lista.digitosMasGrandes().toString());
+        cogerFoco();
+    }
+
+    /**
+     *   Muestra en el area de texto el valor que corresponde al segundo minimo
+     *  
+     */
+    private void  segundoMinimo()
+    {
+        clear();     
+        if (lista.estaVacia())
+        {
+            areaTexto.append("Lista vacia");
+        }
+        else
+        {
+            mostrarLista();
+            areaTexto.append("\n2 minimo: " + lista.segundoMinimo() + "\n");
+        }
+        cogerFoco();
+    }
+
+    /**
+     *  Borra los segundos minimos y muestra en el area de texto la lista
+     *  y la cantidad de segundos minimos borrados
+     */
+    private void  borrarSegundosMinimos()
+    {
+        clear(); 
+        mostrarLista();
+        int borrados = lista.borrarSegundosMinimos();
+        areaTexto.append("\nSe han borrado " + borrados + " elementos de la lista\n");
+        mostrarLista();
+        cogerFoco();
+    }
+
+    /**
+     * ordena la lista y la muestra en el area de texto
+     */
+    private void ordenar()
+    {
+        clear();
+        int[] ordenada = lista.ordenar();
+        mostrarLista();        
+        areaTexto.append("\nCopia ordenada de la lista " + Arrays.toString(ordenada) + "\n");
+
+    }
+
+    /**
+     * mostrar la lista y su n de elementos en el area de texto
+     */
+    private void mostrarLista()
+    {
+        clear();
+        areaTexto.append("Lista: " + lista.toString());
+        areaTexto.append("\nN real elementos en lista " + lista.getTotalNumeros());
+        cogerFoco();
+
+    }
+
+    /**
+     * Vacia la lista
+     */
+    private void vaciarLista()
+    {
+        clear();
+        lista.vaciarLista();         
+        areaTexto.append("Borrados todos los valores de la lista");
+        cogerFoco();
+
+    }
+
+    /**
+     * finalizar aplicacion
+     */
+    private void salir()
+    {
+        System.exit(0);
+
+    }
+
+    /**
+     * llevar el foco al campo de texto y seleccionar todo
+     */
+    private void cogerFoco()
+    {
+        txtNumero.requestFocus();
+        txtNumero.selectAll();
+
+    }
+
+    /**
+     * limpiar area de texto
+     */
+    private void clear()
+    {
+        areaTexto.setText("");
+        cogerFoco();
+    }
+
+    /**
+     * Punto entrada de la aplicaicon
+     */
+    public static void main(String[] args)
+    {
+        int tam = (int) (Math.random() * 6) +  5;
+        ListaNumeros lista = new ListaNumeros(tam);
+        int[] lis = {12,  12,  12,  4,  4,  55, 789}; //{4, 4, 3, 3, 3, 7, 7, 8};
+        for (int valor:lis)
+            lista.addElemento(valor);
+        new GuiListaNumeros("Lista de numeros", lista);
+
+    }
+
+}
---
+++ UT5/Entrega 1 UT5 Lista de números AL/ListaNumeros.java
@@ -0,0 +1,199 @@
+import java.util.Arrays;
+
+/**
+ * La clase representa a una lista de 
+ * numeros enteros
+ * 
+ */
+ 
+public class ListaNumeros{
+
+    private int[] lista;  // la lista de elementos
+    private int pos;  // n real de elementos en la lista
+
+    /**
+     * Constructor de la clase ListaNumeros
+     * @param n el tamano maximo de la lista
+     */
+    public ListaNumeros(int n){
+        lista = new int[n];
+        pos = 0;
+    }
+
+    /**
+     * @param  numero valor que se anade al final
+     * de la lista. No se hace nada si la lista esta completa
+     * @return true si se ha podido anadir, false en otro caso
+     */
+    public boolean addElemento(int numero){
+        if(estaCompleta()){
+        	return false;
+        }else{
+        	lista[pos] = numero;
+        	pos++;
+        	return true;
+        }
+    }
+
+    /**
+     * devuelve true si la lista esta completa, false en otro caso
+     * Hazlo sin if
+     */
+    public boolean estaCompleta(){
+        return pos == lista.length;
+    }
+
+    /**
+     * devuelve true si la lista esta vacia, false en otro caso
+     * Hazlo sin if
+     */
+    public boolean estaVacia(){
+        return pos == 0;
+    }
+
+    /**
+     * @return  devuelve el n de elementos
+     * realmente guardados en la lista 
+     */
+    public int getTotalNumeros(){
+        return pos;     
+    }
+
+    /**
+     * Representacion textual de la lista de la forma
+     * Si lista = {175, 826, 89, 9, 41, 13, 72, 11, 23, 107} devuelve
+     * |  175 | 826 | 89 | 9 | 41 | 13 | 72 | 11 | 23 |  107 |
+     * 
+     * Si la lista esta vacia devuelve ""
+     */
+    public String toString(){
+       	String aux = "|";
+       	for(int i = 0; i < pos; i++){
+       		aux += " " + lista[i] + " |";
+       	}
+       	return aux;
+    }
+
+    /**
+     *  Vacia la lista
+     */
+    public void vaciarLista(){
+        this.pos = 0;
+    }
+
+    /**
+     *  
+     * @return   el segundo minimo de la lista
+     * Asumimos que la lista tiene al menos un elemento
+     * 
+     * 
+     * Si lista = {12,  12,  12,  4,  4,  55, 789} el 2 minimo es 12
+     * Si lista = {4, 4, 3, 3, 3, 7, 7, 8} el 2 minimo es 4
+     * Si lista = {4, 4} el 2 minimo es 4
+     * Si lista = {4, 3, 2, -7, -5} el 2 minimo es -5
+     */
+    public int segundoMinimo(){
+    	int[] aux = ordenar();
+    	for(int i = 0; i < aux.length; i++){
+    		if(aux[i] > aux[0])
+    			return aux[i];
+    	}
+    	return aux[0];
+    }
+
+    /**
+     *  Borra todos los segundos minimos de la lista y devuelve el total de
+     *    borrados. Sin utilizar arrays adicionales
+     *  
+     */
+    public int borrarSegundosMinimos(){
+    	int aux = segundoMinimo();
+    	int contador = 0;
+       	for(int i = 0; i < pos; i++){
+       		if(lista[i] == aux){
+       			for(int j = i; j < pos-1; j++){
+       				lista[j] = lista[j+1];
+       			}
+       			contador++;
+       		}
+
+       	}
+       	pos -= contador;
+    	return contador;
+    }
+
+    /**
+     *  Calcula y devuelve un nuevo objeto ListaNumeros  
+     *  Este objeto contiene tambien un array con tantos elementos como hay en el array lista
+     *  pero en cada posicion queda el digito mayor de cada uno de 
+     *  los elementos de lista
+     *  
+     *  Si lista contiene {175, 826, 89, 9, 41, 13, 72, 11, 23, 107}  devuelve
+     *  {7, 8, 9, 9, 4, 3, 7, 1, 3, 7}
+     *  
+     *  Hay que utilizar el metodo recursivo digitoMasGrande()
+     */
+    public  ListaNumeros digitosMasGrandes(){
+        ListaNumeros aux =  new ListaNumeros(pos);
+        for(int i = 0; i < pos; i++){
+        	aux.addElemento(digitoMasGrande(lista[i]));
+        }
+        return aux;
+    }
+
+    /**
+     *  Metodo estatico recursivo que devuelve el digito mayor de n (siempre se
+     *  devuelve digito positivo)
+     *  Si n = 3263 devuelve 6, si n = 9878 devuelve 9, si n = 5 devuelve 5
+     *  Si n = -3263 devuelve 6
+     */
+    private static int digitoMasGrande(int n){
+        n = Math.abs(n); // para que la cifra devuelta siempre sea positiva
+        if(n/10 == 0)
+        	return n;
+        if(n%10 > digitoMasGrande(n/10))
+        	return n%10;
+        else
+        	return digitoMasGrande(n/10);
+    }
+
+
+    /**
+     *  Devuelve una copia del array lista ordenada
+     *  Usa solo metodos de la clase  Arrays
+     *   
+     *
+     */
+    public int[] ordenar(){
+     	int[] aux = Arrays.copyOf(lista, pos);
+     	Arrays.sort(aux);
+     	return aux;
+    }
+
+    /**
+     * Mostrar en pantalla la lista
+     */
+    public void escribirLista(){
+        System.out.println(this.toString());
+    } 
+
+    public static void main(String[] args) {
+    	ListaNumeros demo = new ListaNumeros(14);
+    	for(int i = 0; i < 11; i++){
+    		demo.addElemento((int) (Math.random() * -300) + 150);
+    	}
+    	demo.addElemento(-10);
+    	demo.addElemento(-10);
+    	demo.escribirLista();
+    	System.out.println(Arrays.toString(demo.ordenar()));
+    	System.out.println(demo.segundoMinimo());
+    	System.out.println(demo.borrarSegundosMinimos());
+    	demo.escribirLista();
+    	int numero = (int) (Math.random() * -9876) + 5678;
+		System.out.println(numero);
+    	System.out.println(digitoMasGrande(numero));
+    	ListaNumeros listaDigitosMasGrandes = demo.digitosMasGrandes();
+    	listaDigitosMasGrandes.escribirLista();
+    }
+
+}
